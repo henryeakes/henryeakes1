02c50f12660a086cbc23e2538f6432f2abfc15f3
henryeakes <henry.eakes@gmail.com>
2026-01-28 14:37:57 -0500

    Create app/layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Henry Eakes",
+  description: "College & NFL Football",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
